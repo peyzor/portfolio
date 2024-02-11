2bd463cde24991832d83fd3775f5a0c1082c39b3
Peyman <hassani.peyman627@gmail.com>
2024-02-11 22:13:13 +0330

    Add ping route

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/peyzor/portfolio
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello portfolio")
+	})
+
+	port := 8080
+	fmt.Printf("Server is listening on port: %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+}
